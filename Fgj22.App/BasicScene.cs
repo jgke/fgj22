d562fbdfb487e2faccd972a0cdb164873c8217c8
Jaakko Hannikainen <jgke@jgke.fi>
2022-01-29 09:49:23 +0200

    Add enemy spawn points

--- Fgj22.App/BasicScene.cs
+++ Fgj22.App/BasicScene.cs
@@ -13,24 +13,37 @@ namespace Fgj22.App
         {
             base.Initialize();
 
-            // default to 1280x720 with no SceneResolutionPolicy
-            SetDesignResolution(1280, 720, SceneResolutionPolicy.None);
-            Screen.SetSize(1280, 720);
+            // Set the playing area size
+            SetDesignResolution(480, 320, SceneResolutionPolicy.ShowAllPixelPerfect);
+            // Set the rendering resolution
+            Screen.SetSize(1440, 1280);
 
             var editor = CreateEntity("editor", new Vector2(300, 100));
             editor.AddComponent(new Editor());
 
-            var playerEntity = CreateEntity("player", new Vector2(Screen.Width / 2 - 200, Screen.Height / 2 - 200));
+            var map = Content.LoadTiledMap("Content/tiledMap.tmx");
+			var playerSpawn = map.GetObjectGroup("objects").Objects["spawn"];
+			var playerSpawnPosition = new Vector2(playerSpawn.X, playerSpawn.Y);
+            var playerEntity = CreateEntity("player", playerSpawnPosition);
             playerEntity.AddComponent(new Test());
             playerEntity.AddComponent(new BoxCollider(-8, -16, 16, 32));
-            var map = Content.LoadTiledMap("Content/tiledMap.tmx");
             playerEntity.AddComponent(new TiledMapMover(map.GetLayer<TmxLayer>("main")));
 
             var tiledEntity = CreateEntity("tiled-map-entity");
             tiledEntity.AddComponent(new TiledMapRenderer(map, "main"));
 
-            var enemy = CreateEntity("enemy", new Vector2(Screen.Width / 2 - 200, Screen.Height / 2 - 100));
-            enemy.AddComponent(new Enemy("standard"));
+			var enemySpawns = map.GetObjectGroup("enemies").Objects;
+			foreach (TmxObject itemSpawnPoint in enemySpawns)
+			{
+				var enemy = CreateEntity("enemy", new Vector2(itemSpawnPoint.X, itemSpawnPoint.Y));
+                enemy.AddComponent(new Enemy("standard"));
+			}
+
+            Camera.Entity.AddComponent(new FollowCamera(playerEntity));
+			var topLeft = new Vector2(0, 0);
+			var bottomRight = new Vector2(map.TileWidth * map.Width, map.TileWidth * map.Height);
+			tiledEntity.AddComponent(new CameraBounds(topLeft, bottomRight));
+            tiledEntity.AddComponent(new CameraBounds(topLeft, bottomRight));
         }
     }
 }
